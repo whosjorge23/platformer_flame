372713403331986ccac8c8770308410b04202a81
Giorgio Giannotta <jorge_upa@msn.com>
2023-09-16 11:05:59 +0200

    Add level and camera

---
+++ lib/levels/level.dart
@@ -0,0 +1,13 @@
+import 'package:flame/components.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+
+class Level extends World {
+  late TiledComponent level;
+
+  @override
+  Future<dynamic> onLoad() async {
+    level = await TiledComponent.load('Level-01.tmx', Vector2(16, 16));
+    add(level);
+    return super.onLoad();
+  }
+}
