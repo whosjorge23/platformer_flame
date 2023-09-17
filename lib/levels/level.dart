b4bf79a9180d06990216da5119d841eb7ae6a03e
Giorgio Giannotta <jorge_upa@msn.com>
2023-09-17 08:59:44 +0200

    Add player movements + joypad

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -4,8 +4,9 @@ import 'package:platform_game/actors/player.dart';
 
 class Level extends World {
   final String levelName;
+  final Player player;
 
-  Level({required this.levelName});
+  Level({required this.levelName, required this.player});
 
   late TiledComponent level;
 
@@ -17,7 +18,7 @@ class Level extends World {
     for (final spawnPoint in spawnPointsLayer!.objects) {
       switch (spawnPoint.class_) {
         case 'Player':
-          final player = Player(character: 'Ninja Frog', position: Vector2(spawnPoint.x, spawnPoint.y));
+          player.position = Vector2(spawnPoint.x, spawnPoint.y);
           add(player);
       }
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/foundation.dart';
@@ -9,9 +11,16 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
+  bool isMobile;
 
-  PixelAdventure game = PixelAdventure();
+  if (Platform.isAndroid || Platform.isIOS) {
+    isMobile = true;
+  } else {
+    isMobile = false;
+  }
+
+  PixelAdventure game = PixelAdventure(showJoystick: isMobile);
   runApp(
-    GameWidget(game: kDebugMode ? PixelAdventure() : game),
+    GameWidget(game: kDebugMode ? PixelAdventure(showJoystick: isMobile) : game),
   );
 }
